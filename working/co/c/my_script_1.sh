30b53eb77c776346ca432b2b38df226373233a79
Kyle Mills <kmills@mcs11.science.uoit.ca>
2014-06-13 09:44:33 -0400

    my_script_1.sh seems to work.

--- working/co/c/my_script_1.sh
+++ working/co/c/my_script_1.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 
-PWSCF='/home/kmills/espresso-5.0/bin/pw.x  -nk 1 -nd 1 -nb 1 -nt 1 '
+PREFIX='co'
 
+
+PWSCF='/home/kmills/espresso-5.0/bin/pw.x  -nk 1 -nd 1 -nb 1 -nt 1 '
+PW2BGW='/home/kmills/espresso-5.0/bin/pw2bgw.x '
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+pwd
 cd 1-scf
-ln ../../*.UPF ./
+ln -s ../../*.UPF ./
 $PWSCF < in > out
+cd ..
+ln -s "../1-scf/${PREFIX}.save/" "2-wfn/"
+cd 2-wfn/
+$PWSCF < in > out
+$PW2BGW < pp_in > pp_out
 
 
 
