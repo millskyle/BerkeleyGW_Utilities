159b0bcfe3545c1a6c72255c658d1239c5a104c9
Kyle Mills <kmills@mcs11.science.uoit.ca>
2014-06-13 15:04:28 -0400

    template

--- working/co/co/link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm run.sh
-rm cleanup.sh
-ln -s ../run.sh run.sh
-ln -s ../cleanup.sh cleanup.sh
-
---
+++ working/co/template/link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm run.sh
+rm cleanup.sh
+ln -s ../run.sh run.sh
+ln -s ../cleanup.sh cleanup.sh
+
