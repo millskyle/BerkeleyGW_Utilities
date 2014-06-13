eec7f64c79cb6ffa0a4424dc42ae6a026641357e
Kyle Mills <kmills@mcs11.science.uoit.ca>
2014-06-13 09:53:37 -0400

    fixed some problems

--- working/co/c/my_script_1.sh
+++ working/co/c/my_script_1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+currentdir="`pwd`"
 
-PREFIX='co'
 
+PREFIX=`tr -d '\n' < prefix`
+echo "Prefix set to $PREFIX"
 
 PWSCF='/home/kmills/espresso-5.0/bin/pw.x  -nk 1 -nd 1 -nb 1 -nt 1 '
 PW2BGW='/home/kmills/espresso-5.0/bin/pw2bgw.x '
@@ -11,15 +13,23 @@ PW2BGW='/home/kmills/espresso-5.0/bin/pw2bgw.x '
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
-pwd
+echo "Root directory is $DIR"
+
 cd 1-scf
 ln -s ../../*.UPF ./
+echo "Running first PWSCF calculation"
 $PWSCF < in > out
 cd ..
 ln -s "../1-scf/${PREFIX}.save/" "2-wfn/"
 cd 2-wfn/
+echo "Running second PWSCF calculation"
 $PWSCF < in > out
+echo "Converting PWSCF binary wavefunction to BerkeleyGW WFN"
 $PW2BGW < pp_in > pp_out
 
 
+mv wfn.complex WFN_$PREFIX
 
+
+
+cd $currentdir
